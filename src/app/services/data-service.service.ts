b80e80fba9eda7f84b76d72bf6d8cd2a38726d95
Hemant <hsr7047@gmail.com>
2021-12-01 01:07:35 +0530

    Get_Data

--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataServiceService {
 
-  constructor(private http:HttpClient) { }
+  private globalDataUrl ='https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/11-28-2021.csv';
+  constructor(private  http : HttpClient) { }
+  getGlobalData(){
+    return this.http.get(this.globalDataUrl,{responseType:'text'}).pipe (
+      map(result=>{
+       return result;
+      })
+    )
+  }
 }
+ 
